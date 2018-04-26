23a568fbb06389042945e6d71a1268a96b5b261e
Sportsdawg1 <Sportsdawg1@users.noreply.github.com>
2018-04-25 20:55:19 -0700

    completed 2 recipes - evilpolygons and code4life

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -7,18 +7,25 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class Code4Life {
-	
+	public static void main(String [] args) {
 	//1.  Ask the user how many hours they spent coding this week.
-	
+	String stringHours = JOptionPane.showInputDialog("how many hours");
+	int hours = Integer.parseInt(stringHours);
 	//2.  If it is less than or equal to 2, tell them to stop watching YouTube and write code instead.
-	
+	if (hours <= 2) {
+		JOptionPane.showMessageDialog(null, "stop watching youtube and code");
+	} else if (hours >= 3 && hours <= 5) {
 	//3.  If it is greater than or equal to 3 AND less than or equal to 5, tell them they're a Code Ninja
-	
+		JOptionPane.showMessageDialog(null, "code ninja");
+	} else if (hours > 5) {
 	//4.  If it is more than 5, call the method below to play the Batman theme song.  
-
+		playBatmanTheme();
+	}
+	}
     private static void playBatmanTheme() {
     	try {
    		 Clip clip = AudioSystem.getClip();
--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JOptionPane;
 
@@ -7,22 +9,50 @@ import org.jointheleague.graphical.robot.Robot;
 public class EvilPolygonsChallenge {
 	
 	public static void main(String[] args) {
-		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		screenSize.setSize(1000, 1000);
 		//1. Create a new Robot
-
+		Robot rob = new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
+		rob.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"red","green", "blue", "yellow"}, 0);
+		System.out.println(colorChoice);
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+	    if (colorChoice == 3) {
+	    	rob.setPenColor(255,255,0);
+	    } else if (colorChoice == 2) {
+			rob.setPenColor(0,0,255);
+		} else if (colorChoice == 1) {
+			rob.setPenColor(0,255,0);
+		} else if (colorChoice == 0) {
+			rob.setPenColor(255,0,0);
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String numPolygonsS = JOptionPane.showInputDialog("how many polygons(this number will be multiplied by 16)");
+		int numPolygons = Integer.parseInt(numPolygonsS);
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		rob.setX(10);
+		rob.setY(10);
+		rob.setAngle(90);
+		for(int b = 0;b < 4; b++) {
+		for(int c = 0; c < numPolygons; c++) {
+		for(int i = 0; i < 4; i++) {
+			rob.penDown();
+			for(int a = 0; a < 4; a++) {
+				rob.move(10);
+				rob.turn(90);
+			}
+			rob.penUp();
+			rob.move(100);
+			rob.turn(90);
+		}
+		rob.move(205);
 		//6. Make it so your shapes do not overlap
-
+		}
+			rob.turn(90);
+		}
+		}
 		//7. Challenge: add more colors to the Option Dialog.
 	}
-}
+
 
